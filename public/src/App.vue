7c3e6c003db45dc1179db339655a5840e586023d
Marcosmm1 <marcos-ubuntu@Marcos.localdomain>
2020-04-22 22:08:26 +0100

    Vuetify rdy for start programming!

--- public/src/App.vue
+++ public/src/App.vue
@@ -1,9 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    HOlA QUE HACE
+    </v-app-bar>
+
+    <v-content>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
